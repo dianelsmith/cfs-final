91a60cda7ce61ed3e147f108864bb339812757cc
Eric Siebeneich <eric.siebeneich@codercamps.com>
2015-08-14 09:51:20 -0500

    Added problem 3

---
+++ finalReview/Problem3/problem3.js
@@ -0,0 +1,15 @@
+﻿/*
+Problem 3:
+
+The html contains a left div, and a right div.
+When the user clicks the ball, it should go to the other div
+
+Suggestion: Maybe use jQuery.delegate?
+
+If you're stumped, try getting something to log to the console
+if the ball is clicked, THEN worry about moving the ball div.
+*/
+
+$(document).ready(function() {
+
+});
