3ad3bd00892df8d52d1256a1c59af6746bd15d85
Eric Siebeneich <eric.siebeneich@codercamps.com>
2015-08-14 10:35:16 -0500

    Problem 5

---
+++ finalReview/Problem5/problem5.js
@@ -0,0 +1,32 @@
+﻿/*
+Problem 5: Objects
+
+Don't modify the document ready function. Just add the Cat object. Check your spelling!
+
+Create an cat object!
+It should have color and weight properties,
+as well as be able to meow (function)
+*/
+
+/*
+Write the Cat object here
+*/
+
+$(document).ready(function() {
+    var cat = new Cat();
+
+    $('#colorPicker').on('click', function() {
+        cat.color = $('#color-input').val();
+        $('.catBlend').css('background-color', cat.color);
+    });
+
+    $('#weight').on('click', function() {
+        cat.weight = $('#weight-input').val();
+        $('#weight-output').text(cat.weight);
+    });
+
+    $('.cat').on('click', function() {
+        console.log("The " + cat.color + " cat weighs " + cat.weight + " pounds");
+        cat.meow();
+    })
+});
