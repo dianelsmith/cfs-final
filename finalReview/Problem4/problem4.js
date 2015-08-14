62cf12a8375ab68932bf8b9092639f948d65a064
Eric Siebeneich <eric.siebeneich@codercamps.com>
2015-08-14 10:05:39 -0500

    Problem 4

---
+++ finalReview/Problem4/problem4.js
@@ -0,0 +1,36 @@
+﻿/*
+Problem 4: Functions
+
+Write two functions, getUserInput and setOutput.
+
+Do NOT modify the document ready function.
+*/
+
+$(document).ready(function() {
+  
+    $('#add').on('click', function() {
+        var one = parseInt(getUserInput('.input1'));
+        var two = parseInt(getUserInput('.input2'));
+
+        var output = one + two;
+
+        setOutput('.output1', one);
+        setOutput('.output2', two);
+        setOutput('#output', output);
+    });
+});
+
+/*
+Write the getUserInput function here
+
+Its should only accept 1 parameter, a css selector for the input element
+It should return the value in the input field
+*/
+
+
+
+/*
+Write the setOutput function here
+
+It should accept a two parameters, the css selector and the text used to set
+*/
