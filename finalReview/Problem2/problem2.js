7073740a28b884dad0b6083494ede7dd57215c86
Eric Siebeneich <eric.siebeneich@codercamps.com>
2015-08-14 09:41:28 -0500

    Problem 1 and 2

---
+++ finalReview/Problem2/problem2.js
@@ -0,0 +1,25 @@
+﻿/*
+Problem 2:
+
+jQuery has been imported for you.
+
+A client has contacted you because they've written terrible JavaScript.
+Repair their code! There are 10 bugs in the code. Make a short description of what you've fixed.
+
+1.  ?
+2.  ?
+3.  ?
+4.  ?
+5.  ?
+6.  ?
+7.  ?
+8.  ?
+9.  ?
+10. ?
+*/
+
+document.ready(function {
+    $('nav .ul li').css('list-style-type' square);
+
+    $('.output').text('.p'.text.split( ).length());
+});
